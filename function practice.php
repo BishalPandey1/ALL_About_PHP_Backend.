3a4c69042c21e26532a47ab387427ca6e4ecf1b0
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2024-07-11 20:44:13 +0545

    Update function practice.php

--- function practice.php
+++ function practice.php
@@ -11,7 +11,6 @@ function setHeight($minheight = 50) {
   function familyName($fname, $year) {
     echo "$fname Refsnes. Born in $year <br>";
   }
-  
   familyName("Hege", "1975");
   familyName("Stale", "1978");
   familyName("Kai Jim", "1983");
